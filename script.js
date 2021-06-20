3cc7c7e9b66cdbffd112313d9c9cb78816ef9704
Md. Asif Joardar <mrsparrow04@gmail.com>
2021-06-20 22:14:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let submitTodo = document.querySelector('.input-things');
-let newTask = document.querySelector('#new-task');
+"use strict";
+
+let submitTodo = document.querySelector('.input-tasks');
+let submitUpdate = document.querySelector('.input-updates');
 let addTodo = document.querySelector('.add-todo');
 let completeTodo = document.querySelector('.complete-todo');
+let addHistory = document.querySelector('#history-items');
+let getTask = document.querySelector('#new-task');
+let getPriority = document.querySelector('#priority-select');
+let getUpdatePriority = document.querySelector('#update-priority');
+let getUpdateTask = document.querySelector('#update-task');
 
-let selected = document.querySelector(".selected");
-let optionsContainer = document.querySelector(".options-container");
-let optionsList = document.querySelectorAll(".option");
-
+let history = [];
 let todo = {};
 let complete = {};
 
+if(JSON.parse(localStorage.getItem('history')) != null)
+    history = JSON.parse(localStorage.getItem('history'));
 if(JSON.parse(localStorage.getItem('todo')) != null)
     todo = JSON.parse(localStorage.getItem('todo'));
 if(JSON.parse(localStorage.getItem('complete')) != null)
@@ -17,24 +23,9 @@ if(JSON.parse(localStorage.getItem('complete')) != null)
 
 /*..id genarator..*/
 const id = function () {
-    return '_' + Math.random().toString(36).substr(2, 7);
+    return Math.random().toString(36).substr(2, 7);
 };
 /*....*/
-
-/*..dropdown..*/
-function dropDown(){
-    selected.addEventListener("click", () => {
-        optionsContainer.classList.toggle("active");
-    });
-
-    optionsList.forEach(o => {
-        o.addEventListener("click", () => {
-            selected.innerHTML = o.querySelector("label").innerHTML;
-            optionsContainer.classList.remove("active");
-        });
-    });
-}
-/*.............................*/
 
 /*..create todo list..*/
 let createtask = function(task, priority, ID){
@@ -55,6 +46,7 @@ let createtask = function(task, priority, ID){
     listItem.className = 'item';
     listItem.className += " " + priority;
     btn.className = 'update';
+    
     label.innerText = task;
     checkBox.checked = false;
 
@@ -99,27 +91,37 @@ let createCompleteTask = function(text, priority, ID){
 }
 /*.............................*/
 
+let createHistoryList = function(text){
+    let listItem = document.createElement('li');
+    listItem.className = 'item';
+    listItem.innerText = text;
+    return listItem;
+}
+
 /*..adding new todo into localstorage and todo list(listenning event from submit)..*/
 let addTask = function(event) {
     event.preventDefault();
-    if(selected.innerHTML === "Priority" || newTask.value == ""){
+    if(getPriority.value === "" || getTask.value == ""){
         alert(`Priority or input field should not empty.`);
         return;
     }
     var ID = id().toString();
     todo[ID] = {
-        text: newTask.value,
-        priority: selected.innerHTML,
+        text: getTask.value,
+        priority: getPriority.value,
     }
-    console.log(todo);
     localStorage.setItem('todo', JSON.stringify(todo));
 
-    let listItem = createtask(newTask.value, selected.innerHTML, ID);
+    let listItem = createtask(getTask.value, getPriority.value, ID);
     addTodo.appendChild(listItem);
     
-    newTask.value = "";
-    selected.innerHTML = "Priority";
-    bindCompleteItems1(listItem, completeTask, /*todo.length-1*/ID);
+    getTask.value = "";
+    getPriority.value = "";
+    let historyListItem = createHistoryList(`Id "${ID}" is Added`);
+    addHistory.insertBefore(historyListItem, addHistory.childNodes[0]);
+    history.push(`Id "${ID}" is Added`);
+    localStorage.setItem('history', JSON.stringify(history));
+    bindCompleteItems1(listItem, completeTask, ID);
 }
 /*.............................*/
 
@@ -134,7 +136,46 @@ let undoAdd = function(ID){
     delete complete[ID];
     localStorage.setItem('complete', JSON.stringify(complete));
 
+    let historyListItem = createHistoryList(`Undo Id "${ID}"`);
+    addHistory.insertBefore(historyListItem, addHistory.childNodes[0]);
+    history.push(`Undo Id "${ID}"`);
+    localStorage.setItem('history', JSON.stringify(history));
     bindCompleteItems1(listItem, completeTask, ID);
+}
+/*.............................*/
+
+/*updating task*/
+let updateTask = function(listItem, ID){
+    document.querySelector('.task-container').hidden = true;
+    document.querySelector('.update-container').hidden = false;
+    document.querySelector('#update-task').value = todo[ID].text;
+    document.querySelector('#update-priority').value = todo[ID].priority;
+    document.querySelector('.update-title').innerText = `Update Your task for ID: ${ID}`;
+
+
+    submitUpdate.addEventListener('submit', function(event){
+        if(getUpdatePriority.value === "" || getUpdateTask.value == ""){
+            alert(`1 Priority or input field should not empty.`);
+            return;
+        }
+        listItem.classList.remove(todo[ID].priority);
+
+        todo[ID].text = getUpdateTask.value;
+        todo[ID].priority = getUpdatePriority.value;
+        localStorage.setItem('todo', JSON.stringify(todo));
+
+        listItem.querySelector('label').innerText = todo[ID].text;
+        listItem.className += " " + todo[ID].priority;
+        document.querySelector('.task-container').hidden = false;
+        document.querySelector('.update-container').hidden = true;
+
+        let historyListItem = createHistoryList(`Id "${ID}" is Updated`);
+        addHistory.insertBefore(historyListItem, addHistory.childNodes[0]);
+        history.push(`Id "${ID}" is Updated`);
+        localStorage.setItem('history', JSON.stringify(history));
+
+        bindCompleteItems1(listItem, completeTask, ID);
+    });
 }
 /*.............................*/
 
@@ -166,12 +207,22 @@ let completeTask = function(ID){
 
     completeTodo.appendChild(listItem);
 
+    let historyListItem = createHistoryList(`Id "${ID}" completed`);
+    addHistory.insertBefore(historyListItem, addHistory.childNodes[0]);
+    history.push(`Id "${ID}" completed`);
+    localStorage.setItem('history', JSON.stringify(history));
+
     bindCompleteItems2(listItem, deleteTask, ID);
 }
 /*.............................*/
 
 /*..listenning to delete button..*/
 let deleteTask = function(ID) {
+    let historyListItem = createHistoryList(`Delete completed Id "${ID}"`);
+    addHistory.insertBefore(historyListItem, addHistory.childNodes[0]);
+    history.push(`Delete completed Id "${ID}"`);
+    localStorage.setItem('history', JSON.stringify(history));
+
     delete complete[ID];
     localStorage.setItem('complete', JSON.stringify(complete));
 }
@@ -198,33 +249,31 @@ let bindCompleteItems1 = function(listItem, checkBoxclick, ID) {
     let update = listItem.querySelector('button[class="update"]');
     checkBox.addEventListener('click', function(){
         listItem.remove();
-        checkBoxclick(ID);
+        checkBoxclick(ID.toString());
     });
     update.addEventListener('click', function(){
-        listItem.remove();
-        newTask.value = todo[ID].text;
-        selected.innerText = todo[ID].priority;
-        delete todo[ID];
-        localStorage.setItem('todo', JSON.stringify(todo));
-        init();
+        updateTask(listItem, ID);
     });
-
 }
 /*.............................*/
 
 function init(){
-    dropDown();
-    /*..listen to submitted todo item..*/
+    /*..listen to submitted or updated todo item..*/
     submitTodo.addEventListener('submit', addTask);
     /*.............................*/
 
     /*..traverse on prev todo and complete list..*/
-    for(/*let i=0;i<todo.length;i++*/ let ID in todo){
+    for(let ID in todo){
         prevTodoList(todo[ID].text, todo[ID].priority, ID);
     }
 
-    for(/*let i=0;i<complete.length;i++*/ let ID in complete){
+    for(let ID in complete){
         prevCompleteList(complete[ID].text, complete[ID].priority, ID);
+    }
+
+    for(let i=0;i<history.length;i++){
+        let historyListItem = createHistoryList(history[i]);
+        addHistory.insertBefore(historyListItem, addHistory.childNodes[0]);
     }
     /*.............................*/
 }
